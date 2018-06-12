03b8bda1d55ac29ac5272c416b216cbd8dff178a
kitomc <kitomc.rd@gmail.com>
2018-06-12 19:43:17 -0400

    aas

--- Prueba/Form1.Designer.cs
+++ Prueba/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(473, 79);
+            this.button1.Location = new System.Drawing.Point(489, 116);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(268, 236);
             this.button1.TabIndex = 0;
